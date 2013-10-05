c435460a9980a1524972631578426ceb3c5fa595
stylesuxx <stylesuxx@gmail.com>
2013-10-05 20:43:19 +0200

    Added functionality for saving and syncing settings

--- js/main.js
+++ js/main.js
@@ -5,18 +5,19 @@ jQuery.noConflict();
   	 * Production code comes here.
   	 */
   	var text = ['This is', 'jquery appending', 'line after line.', '<span class="glyphicon glyphicon-star"></span>'];
-  	var show = function(text, index){
+  	var show = function(text, index) {
   		if(index >= text.length) return;
   		$('#home').append($('<div class="line">' + text[index] + '</div>').fadeIn(2000, function(){
   			show(text, index+1);
   		}));
   	}
 
-	$('#myTab a').click(function (e) {
-	  e.preventDefault()
-	  $(this).tab('show')
-	});
+  	$('#myTab a').click(function (e) {
+  	  e.preventDefault()
+  	  $(this).tab('show')
+  	});
 
+    // Setting the badge text when the button is pressed
   	$('.set-badge').click(function() {
   		chrome.browserAction.setBadgeText({'text': '1111'});
   	});
@@ -25,6 +26,15 @@ jQuery.noConflict();
   		chrome.browserAction.setBadgeText({'text': ''});
   	});
 
-  	show(text, 0);
+  	var l = function(message) {
+      console.log('Log:', message);
+    }
+
+    // Initializing the settings
+    settings = new Settings($, 'form.settings');
+    settings.get('text-field', l);
+
+    // Show some text
+    show(text, 0);
   });
 })(jQuery);
---
+++ js/settings.js
@@ -0,0 +1,89 @@
+/**
+ * Settings Storage
+ * 
+ * The user settings are stored in the chrome storage and are synced between browsers
+ * where sync is enabled.
+ *
+ * When the save button is clicked, all current values are stored to the chrome storage.
+ * When the cancel button is clicked, the current values are overwritten by the last
+ * that were saved.
+ *
+ * Values from the storage can be passed to a callback via get(key, callback).
+ */
+var Settings = function(jQuery, form) {
+  var $ = jQuery;
+  var $settings = $(form);
+
+  // Save and sync all settings
+  var save = function(){
+    var options = {};
+
+    // Processing all text and select inputs
+    $('input[type="text"], select', $settings).each(function(index, item) {
+      options[$(item).attr('name')] = $(item).val();
+    });
+
+    // Processing radio inputs
+    $('input[type="radio"]', $settings).each(function(index, item) {
+      if($(item).is(":checked")){
+        options[$(item).attr('name')] = $(item).val();
+      }
+    });
+
+    // Processing all checkboxes
+    $('input[type="checkbox"]', $settings).each(function(index, item) {
+      options[$(item).attr('name')] = ($(item).is(":checked")) ? true : false;
+    });
+
+    // Syncing the data with the storage
+    chrome.storage.sync.set(options, function() {
+      console.log('Saved the settings');
+    });
+  };
+
+  // Initialize the settings
+  var initialize = function() {
+    chrome.storage.sync.get(function(e) {
+      var $item = null;
+      $.each(e, function(key, value) {
+        $item = $('[name="' + key + '"]', $settings);
+        if($item.length > 0){
+          if($item[0].type == 'text' || $item[0].type == 'select-one'){
+            $item.val(value);
+          }
+          
+          else if($item[0].type == 'checkbox'){
+            if(value){
+              $item.prop('checked', true);
+            }
+          }
+
+          else {
+            $('[name="' + key + '"][value="' + value + '"]', $settings).prop('checked', true);
+          }
+        }
+      });
+    });
+  };
+
+  // Pass a value by its key to a callback function
+  this.get = function(key, callback) {
+    var value = chrome.storage.sync.get(key, function(e) {
+      callback(e[key]);
+    });
+  }
+
+  // Action when the save button is clicked
+  $('.save-settings').click(function(e) {
+    e.preventDefault();
+    save();
+  });
+
+  // Action when the cancel button is clicked
+  $('.cancel-settings').click(function(e) {
+    e.preventDefault();
+    initialize();
+  });
+
+  initialize();
+};
